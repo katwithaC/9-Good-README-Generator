514955507581c717dee34b54b0dbe25b14b8678e
katwithaC <catherinerosearredondo@gmail.com>
2020-12-09 17:37:45 -0600

    created readme file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var inquirer = require("inquirer");
 var fs = require('fs');
 const util = require('util');
-const generateMarkdown = ('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 
 
