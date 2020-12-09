6c75a20cc6580e84ab319ccb8e04d52f7bda289e
katwithaC <catherinerosearredondo@gmail.com>
2020-12-09 04:05:05 -0600

    adding array of questions for user index.js

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
+var inquirer = require("inquirer");
+var fs = require('fs');
+const util = require('util');
+
+
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: 'What is your projects title?',
+        name: 'Title'
+    },
+    {
+        type:'input',
+        message: 'What is the description?',
+        name: 'Description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'Installation'
+    },
+    {
+        type: 'input',
+        message: 'What is the use of the project?',
+        name: 'Usage'
+    },
+    {
+        type: 'checkbox',
+        message: 'What lisence are you using?',
+        name: 'Lisense',
+        options: [
+            'MIT',
+            'Mozilla Public License 2.0',
+            'Apache license 2.0',
+            'ISC',
+            'Microsoft Public License'
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Who is contributing?',
+        name: 'Contributing'
+    },
+    {
+        type: 'input',
+        message: 'Will this be tested?',
+        name: 'Tests'
+    },
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'Questions'
+    }
 
 ];
 
