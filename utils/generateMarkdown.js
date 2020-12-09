43d36f8cdddec1d807e869f34cd69170a5d087ba
katwithaC <catherinerosearredondo@gmail.com>
2020-12-09 16:33:29 -0600

    adding generateMarkdown.js code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,40 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  
+  https://img.shields.io/npm/l/inquirer?style=flat-square
+
+  ## Table of Contents
+
+  * [Description] (#description)
+  * [Installaton] (#installation)
+  * [Usage] (#usage)
+  * [License] (#license)
+  * [Contributing] (#contributing)
+  * [Tests] (#tests)
+  * [Questions] (#questions)
+
+  ## Description:
+  ${data.description}
+
+  ## Installation:
+  ${data.description}
+
+  ## Usage:
+  ${data.usage}
+
+  ## License:
+  ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  ${data.questions}
+
 `;
 }
 
